647900e06d08bfef250541f25178be2b81bb2634
huangqinggai <huangqinggai@qiyi.com>
2017-08-30 17:28:24 +0800

    Chrome extension for remove specified host request.
    Signed-off-by: huangqinggai <huangqinggai@qiyi.com>

---
+++ js/background.js
@@ -0,0 +1,9 @@
+// 要阻塞的网络请求url规则
+var blockUrl = [
+    "*://*.baidustatic.com/*"
+];
+chrome.webRequest.onBeforeRequest.addListener(
+    function(details) { console.log("ddd");return {cancel: true}; },
+    {urls: blockUrl},
+    ["blocking"]
+);
